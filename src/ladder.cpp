a55c176d051c33e2ab47a99db8a529b5fe72aa8a
EmilySun621 <qirans3@uci.edu>
2025-03-16 00:53:47 +0000

    ladder

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -1,14 +1,4 @@
 #include "ladder.h"
-#include <iostream>
-#include <fstream>
-#include <queue>
-#include <set>
-#include <vector>
-#include <string>
-#include <algorithm>
-#include <cmath>
-
-using namespace std;
 
 void error(string word1, string word2, string msg) {
     cerr << "Error: " << msg << " (" << word1 << " -> " << word2 << ")" << endl;
